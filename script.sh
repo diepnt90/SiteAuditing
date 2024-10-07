465879ff9ce0b7e613312115b236f9d2186ce4aa
Diep Nguyen <diep90.nt@gmail.com>
2024-10-07 14:15:52 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,25 +19,27 @@ for dll_file in "$APP_DIR"/*.dll; do
     dll_name=$(basename "$dll_file")
     
     # Check if module_name exists in JSON and tag = 1
-    jq_filter='.[] | select(.module_name == "'"$dll_name"'" and .tag == "1")'
-    module_exists=$(jq "$jq_filter" modules.json)
+    module_exists=$(jq --arg dll_name "$dll_name" '.[] | select(.module_name == $dll_name and .tag == "1")' modules.json)
 
     if [[ -n "$module_exists" ]]; then
         # Update modified_date of the DLL file
         modified_date=$(stat -c %y "$dll_file" | cut -d' ' -f1)
-        jq '(.[] | select(.module_name == "'"$dll_name"'" and .tag == "1") | .modified_date) |= "'"$modified_date"'"' modules.json > temp.json && mv temp.json modules.json
+        jq --arg dll_name "$dll_name" --arg modified_date "$modified_date" \
+           '(.[] | select(.module_name == $dll_name and .tag == "1") | .modified_date) |= $modified_date' modules.json > temp.json && mv temp.json modules.json
         
         # Find version in .deps.json
         for deps_file in "$APP_DIR"/*.deps.json; do
-            version=$(jq -r 'to_entries[] | select(.value.runtime | has("lib/net6.0/'"$dll_name"'")) | .key' "$deps_file" | awk -F '/' '{print $2}')
+            version=$(jq -r --arg dll_name "$dll_name" \
+                      'to_entries[] | select(.value.runtime | has("lib/net6.0/" + $dll_name)) | .key' "$deps_file" | awk -F '/' '{print $2}')
             if [[ -n "$version" ]]; then
-                jq '(.[] | select(.module_name == "'"$dll_name"'" and .tag == "1") | .current_version) |= "'"$version"'"' modules.json > temp.json && mv temp.json modules.json
+                jq --arg dll_name "$dll_name" --arg version "$version" \
+                   '(.[] | select(.module_name == $dll_name and .tag == "1") | .current_version) |= $version' modules.json > temp.json && mv temp.json modules.json
             fi
         done
     else
         # Add new module if it doesn't exist in JSON
-        jq --arg module_name "$dll_name" --arg tag "2" \
-           '. += [{module_name: $module_name, modified_date: "", current_version: "", newest_version: "", links: "", notes: "", tag: $tag}]' \
+        jq --arg module_name "$dll_name" \
+           '. += [{"module_name": $module_name, "modified_date": "", "current_version": "", "newest_version": "", "links": "", "notes": "", "tag": "2"}]' \
            modules.json > temp.json && mv temp.json modules.json
     fi
 done

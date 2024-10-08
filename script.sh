e13599eb72ec64b7b8567a7fbde7c8fc07ae7cac
Diep Nguyen <diep90.nt@gmail.com>
2024-10-08 14:22:38 +0700

    Update script.sh
    the correct one

--- script.sh
+++ script.sh
@@ -53,23 +53,9 @@ done < ./temp_folder/dll_files.txt
 # Step 7: After processing, remove all objects with "tag": "0"
 jq 'del(.[] | select(.tag == "0"))' ./temp_folder/module.json > ./temp_folder/temp.json && mv ./temp_folder/temp.json ./temp_folder/module.json
 
-# Step 7.1: Remove the "Links" key from all objects
-jq '(.[] | del(.links))' ./temp_folder/module.json > ./temp_folder/temp.json && mv ./temp_folder/temp.json ./temp_folder/module.json
-
-# Step 8: Convert module.json to a table format and save as module.txt
-{
-  printf "%-35s | %-20s | %-10s | %-10s | %-60s | %-30s | %-3s\n" "Module Name" "Modified Date" "Curr. Ver" "New. Ver" "Links" "Notes" "Tag"
-  printf "%s\n" "---------------------------------------------------------------------------------------------------------------------------------------------------------"
-
-  jq -r '.[] | [.module_name, .modified_date, .current_version, .newest_version, .links, .notes, .tag] | @tsv' ./temp_folder/module.json | 
-  while IFS=$'\t' read -r module_name modified_date current_version newest_version links notes tag; do
-    printf "%-35s | %-20s | %-10s | %-10s | %-60s | %-30s | %-3s\n" "$module_name" "$modified_date" "$current_version" "$newest_version" "$links" "$notes" "$tag"
-  done
-} > ./temp_folder/module.txt
-
-# Step 9: Upload the updated module.txt to file.io and output the download link
-upload_response=$(curl -F "file=@./temp_folder/module.txt" https://file.io)
+# Step 8: Upload the updated module.json to file.io and output the download link
+upload_response=$(curl -F "file=@./temp_folder/module.json" https://file.io)
 echo "Download link: $(echo $upload_response | jq -r '.link')"
 
-# Step 10: Clean up the temp_folder
+# Step 9: Clean up the temp_folder
 rm -rf ./temp_folder
